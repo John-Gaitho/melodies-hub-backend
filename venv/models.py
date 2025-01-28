dbc210aee91b875a17a58a4ed27e5faf1db6a1ac
John Gaitho <jgaitho016@gmail.com>
2025-01-29 00:05:43 +0300

    song table

--- venv/models.py
+++ venv/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = 
+db = SQLAlchemy
 class User(db.Model):
     __tablename__ = 'user'
 
@@ -8,4 +8,14 @@ class User(db.Model):
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
+# Song model
+class Song(db.Model):
+    __tablename__ = 'song'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(50))
+    release_date = db.Column(db.Date)
+    duration = db.Column(db.Integer)
 
