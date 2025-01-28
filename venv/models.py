116f47ded789579d0ee343f203a0e8a8dde8436e
John Gaitho <jgaitho016@gmail.com>
2025-01-29 00:03:36 +0300

    user table

---
+++ venv/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = 
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
