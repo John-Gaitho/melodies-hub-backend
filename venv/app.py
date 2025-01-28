7aedd9b03b0f8472d288eb7d3ca4e98b9475d1bf
John Gaitho <jgaitho016@gmail.com>
2025-01-29 01:12:12 +0300

    create user

--- venv/app.py
+++ venv/app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 # Importing models
-from models import db
+from models import db, User
 
 db = SQLAlchemy
 # Initializing the Flask app
 app = Flask(__name__)
 app.config.from_object('config.Config')
 migrate = Migrate(app, db)
+
+
+# Routes for User CRUD operations
+
+#to create a new user
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+
+    if User.query.filter_by(username=username).first():
+        return jsonify({'message': 'User with this username already exists'}), 400
+
+    new_user = User(username=username, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully'}), 201
