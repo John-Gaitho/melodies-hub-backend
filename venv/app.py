851b87a29f6f8c9485f668ba94272088b277c924
John Gaitho <jgaitho016@gmail.com>
2025-01-29 09:53:22 +0300

    create &get songs

--- venv/app.py
+++ venv/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 # Importing models
-from models import db, User
+from models import db, User, Song
 
 db = SQLAlchemy
 # Initializing the Flask app
@@ -59,3 +59,24 @@ def delete_user(id):
     db.session.delete(user)
     db.session.commit()
     return jsonify({'message': 'User deleted successfully'})
+
+# to create a new song
+@app.route('/songs', methods=['POST'])
+def create_song():
+    data = request.get_json()
+    title = data.get('title')
+    artist = data.get('artist')
+    genre = data.get('genre')
+    release_date = data.get('release_date')
+    duration = data.get('duration')
+
+    new_song = Song(title=title, artist=artist, genre=genre, release_date=release_date, duration=duration)
+    db.session.add(new_song)
+    db.session.commit()
+    return jsonify({'message': 'Song created successfully'}), 201
+
+# to get all songs
+@app.route('/songs', methods=['GET'])
+def get_songs():
+    songs = Song.query.all()
+    return jsonify([{'id': song.id, 'title': song.title, 'artist': song.artist, 'genre': song.genre} for song in songs])
